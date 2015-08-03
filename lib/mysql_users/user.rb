f10ec97dd5f8ec48c709eee0579d09d0a46f3d88
marguerite <marguerite@pagerduty.com>
2015-08-03 16:45:47 -0400

    Use mysql2 gem as interface to mysql
    We need to be able to escape the strings we interpolate in our SQL. I
    don't want to write this functionality myself, and it was getting a bit
    ridiculous to implement querying of mysql over command line, so I broke
    down and used the gem.

--- lib/mysql_users/user.rb
+++ lib/mysql_users/user.rb
@@ -1,10 +1,29 @@
+require 'mysql2'
+
 module MysqlUsers
   class User
-    def initialize(database, options={})
-      @database = database
+    attr_reader :username
+    attr_reader :db_client
+    attr_reader :scope
+
+    def initialize(db_client, options={})
+      @db_client = db_client
       @username = options[:username]
       @scope = options[:scope]
-      @grants = options[:grants]
+    end
+
+    def exists?
+      result = db_client.query(
+        "SELECT User, Scope FROM mysql.user WHERE "\
+        "User='#{escape(username)}' AND Scope='#{escape(scope)}'"
+      )
+      result.count != 0
+    end
+
+    private
+
+    def escape(string)
+      Mysql2::Client.escape(string)
     end
   end
 end
--- spec/mysql_users/user_spec.rb
+++ spec/mysql_users/user_spec.rb
@@ -1,14 +1,34 @@
 require 'mysql_users'
+require 'mysql2'
 
 RSpec.describe(:user) do
-  let(:database) do
-    double()
-  end
-  let(:user) do
-    MysqlUsers::User.new(database)
+  let(:database_client) do
+    double(Mysql2::Client)
   end
 
-  it 'should work' do
-    user
+  let(:user) do
+    MysqlUsers::User.new(
+      database_client,
+      {
+        username: 'marguerite',
+        scope: '%',
+      },
+    )
+  end
+
+  it 'exists? should return true if that username+scope exists' do
+    allow(database_client).to receive(:query).with(
+      /SELECT User, Scope FROM mysql.user/
+    ).and_return(
+      [{'User' => 'marguerite', 'Scope' => '%'}]
+    )
+    expect(user.exists?).to eq(true)
+  end
+
+  it 'exists? should return false if that username+scope doesn\'t exists' do
+    allow(database_client).to receive(:query).with(
+      /SELECT User, Scope FROM mysql.user/
+    ).and_return([])
+    expect(user.exists?).to eq(false)
   end
 end
