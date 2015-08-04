a11a6c20a30fc5f222bd5e70b35021542393bcc8
marguerite <marguerite@pagerduty.com>
2015-08-04 09:39:51 -0400

    Put exist? tests in a context block

--- spec/mysql_users/user_spec.rb
+++ spec/mysql_users/user_spec.rb
@@ -15,45 +15,49 @@ RSpec.describe(:user) do
     )
   end
 
-  it 'exists? should return true if that username+scope exists' do
-    allow(database_client).to receive(:query).with(
-      /SELECT User, Scope FROM mysql.user/
-    ).and_return(
-      [{'User' => 'marguerite', 'Scope' => '%'}]
-    )
-    expect(user.exists?).to eq(true)
+  context(:exists?) do
+
+    it 'exists? should return true if that username+scope exists' do
+      allow(database_client).to receive(:query).with(
+        /SELECT User, Scope FROM mysql.user/
+      ).and_return(
+        [{'User' => 'marguerite', 'Scope' => '%'}]
+      )
+      expect(user.exists?).to eq(true)
+    end
+
+    it 'exists? should return false if that username+scope doesn\'t exists' do
+      allow(database_client).to receive(:query).with(
+        /SELECT User, Scope FROM mysql.user/
+      ).and_return([])
+      expect(user.exists?).to eq(false)
+    end
+
+    it 'should escape username before interpolating in sql string' do
+      injection = "Robert'; DROP TABLE Students; --"
+      user = MysqlUsers::User.new(
+        database_client,
+        { username: injection, scope: '%' },
+      )
+
+      expect(database_client).to_not receive(:query).with(/bert'/)
+      expect(database_client).to receive(:query).with(/bert\\'/)
+
+      user.exists?
+    end
+
+    it 'should escape scope before interpolating in sql string' do
+      injection = "Robert'; DROP TABLE Students; --"
+      user = MysqlUsers::User.new(
+        database_client,
+        { username: 'marguerite', scope: injection },
+      )
+
+      expect(database_client).to_not receive(:query).with(/bert'/)
+      expect(database_client).to receive(:query).with(/bert\\'/)
+
+      user.exists?
+    end
   end
 
-  it 'exists? should return false if that username+scope doesn\'t exists' do
-    allow(database_client).to receive(:query).with(
-      /SELECT User, Scope FROM mysql.user/
-    ).and_return([])
-    expect(user.exists?).to eq(false)
-  end
-
-  it 'should escape username before interpolating in sql string' do
-    injection = "Robert'; DROP TABLE Students; --"
-    user = MysqlUsers::User.new(
-      database_client,
-      { username: injection, scope: '%' },
-    )
-
-    expect(database_client).to_not receive(:query).with(/bert'/)
-    expect(database_client).to receive(:query).with(/bert\\'/)
-
-    user.exists?
-  end
-
-  it 'should escape scope before interpolating in sql string' do
-    injection = "Robert'; DROP TABLE Students; --"
-    user = MysqlUsers::User.new(
-      database_client,
-      { username: 'marguerite', scope: injection },
-    )
-
-    expect(database_client).to_not receive(:query).with(/bert'/)
-    expect(database_client).to receive(:query).with(/bert\\'/)
-
-    user.exists?
-  end
 end
